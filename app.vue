f3b9ff986834084fe627b25028c8a4121742e131
leehendrick <imhendrick16@gmail.com>
2024-12-20 23:24:25 -0500

    add the avatar image

--- app.vue
+++ app.vue
@@ -45,7 +45,7 @@
         </a>
 
         <a href="#" class="flex items-center hover:text-gray-400">
-
+          <img src="https://avatars.githubusercontent.com/u/115194843?v=4" class="w-5 h-5 rounded-full" alt="Avatar"/>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 ml-1">
             <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
           </svg>
